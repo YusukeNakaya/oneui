a04bf49da2a0f512addcdc6ce3cabb48452dd5db
YusukeNakaya <yusuke.nakaya@jp.oro.com>
2016-06-08 21:39:39 +0900

    Gear

--- js/share.js
+++ js/share.js
@@ -1,3 +1,6 @@
+document.write('<script src="/js/jquery-2.2.4.min.js"></script>');
+document.write('<script src="/js/velocity.min.js"></script>');
+
 window.onload = function() {
   var btn = document.getElementById('ui');
   var timer = (btn.getAttribute('data-timing')) ? parseInt(btn.getAttribute('data-timing')) : 4000;
